97eb5b48360462b912e184e5e1df59215d046e66
mac <mac@macdeMacBook-Pro.local>
2020-06-28 14:07:08 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,14 @@
+public class test {
+//dlkj
+
+    public static void main(String[] args) {
+        int a = 30;
+        System.out.println("hello, mywald");
+        System.out.println("how do you do");
+        System.out.println(a);
+    }
+
+
+}
+// 线性结构
+
